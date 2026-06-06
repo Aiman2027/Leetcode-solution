c03e07733e0af4187adc669be3345708bbb931e5
Aiman Nishat <aimannishat78692@gmail.com>
2026-06-07 01:26:15 +0530

    Time: 3 ms (16.52%), Space: 8.3 MB (52.41%) - LeetHub

--- 0202-happy-number/0202-happy-number.cpp
+++ 0202-happy-number/0202-happy-number.cpp
@@ -1,21 +1,25 @@
 class Solution {
 public:
     bool isHappy(int n) {
-        unordered_set<int> st;
-
-        while (n != 1 && st.find(n) == st.end()) {
+         
+         unordered_set<int>st;
+         
+        while(n != 1 && st.find(n) == st.end()){
             st.insert(n);
 
-            int total = 0;
-            while (n > 0) {
-                int digit = n % 10;
-                total += digit * digit;
-                n /= 10;
+             int ans = 0;
+
+            while(n > 0){
+            int digit = n % 10;
+
+            ans += digit * digit;
+
+            n /=10;
+
             }
-
-            n = total;
-        }
-
-        return n == 1;
-    }
+       n = ans;
+        } 
+     return n == 1;
+        
+}
 };
