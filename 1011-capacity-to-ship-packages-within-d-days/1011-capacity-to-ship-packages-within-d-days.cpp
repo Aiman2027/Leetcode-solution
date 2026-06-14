0eb864755d67ad60af936f077397efaac2190c11
Aiman Nishat <aimannishat78692@gmail.com>
2026-06-14 17:32:14 +0530

    Time: 16 ms (21%), Space: 35.1 MB (52.77%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
@@ -1,37 +1,37 @@
 class Solution {
-   public:
-   int check(vector<int>& weights, int capacity){
-     int days =1 , load =0;
+     public:
 
-     for(int i =0 ;i< weights.size(); i++){
-         if(weights[i] + load > capacity){
-            days +=1;
-            load = weights[i];
-         }
-         else{
-            load +=weights[i];
-         }
+     int check(vector<int>& weights, int capacity){
+         int days = 1, value =0;
+         for(int i=0; i< weights.size();i++){
+            if(weights[i] + value > capacity ){
+                days++;
+                value = weights[i];
+            }
+            else{
+               value += weights[i];  
+            }
+    }
+  return days;
 
      }
-      return days;
-   }
-
 
 public:
     int shipWithinDays(vector<int>& weights, int days) {
-        int low = *max_element(weights.begin() , weights.end());
-        int high = accumulate(weights.begin() , weights.end(), 0);
+        int low = *max_element(weights.begin(), weights.end());
+        int high = accumulate(weights.begin() , weights.end() ,0);
 
         while(low <= high){
-            int mid = low+ (high - low)/2;
+
+            int mid = low+ (high- low)/2;
 
             if(check(weights, mid) <= days){
-                high = mid-1;
+             high = mid-1;
             }
             else{
-                low = mid+1;
+              low = mid+1;
             }
-        }
-        return low;
+            }
+       return low; 
     }
 };
