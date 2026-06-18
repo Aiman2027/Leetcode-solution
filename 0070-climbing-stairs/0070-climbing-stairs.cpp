2d44d669b908fa7d09a38dbe399261e9109b786e
Aiman Nishat <aimannishat78692@gmail.com>
2026-06-18 22:35:39 +0530

    Time: 0 ms (100%), Space: 7.9 MB (71.14%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        if (n == 1) return 1;
+        if (n == 2) return 2;
+        int prev = 1;
+        int next =2;
+
+     for(int i=3;i<=n;i++){
+        int temp = prev + next;
+        prev = next;
+        next = temp;
+        
+        }
+        return next;
+    }
+};
