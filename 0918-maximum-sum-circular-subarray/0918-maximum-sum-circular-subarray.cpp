6331f41e53513423897b8d3d2f8ec6eee9660183
Aiman Nishat <aimannishat78692@gmail.com>
2026-06-25 02:03:49 +0530

    Time: 0 ms (100%), Space: 43.6 MB (78.17%) - LeetHub

--- 0918-maximum-sum-circular-subarray/0918-maximum-sum-circular-subarray.cpp
+++ 0918-maximum-sum-circular-subarray/0918-maximum-sum-circular-subarray.cpp
@@ -1,26 +1,38 @@
 class Solution {
+    public:
+    int maxsubarray(vector<int>& nums){
+        int sum = nums[0];
+        int maxii= nums[0];
+
+        for(int i=1;i<nums.size();i++){
+            sum = max(sum +nums[i] , nums[i]);
+            maxii = max(sum , maxii);
+        }
+       return maxii;
+    }
+    int  minsubarray(vector<int>& nums){
+        int sum =nums[0];
+        int minii = nums[0];
+        for(int i=1;i<nums.size();i++){
+            sum = min(nums[i], sum + nums[i]);
+            minii =  min(minii ,sum);
+        }
+       return minii;
+    }
 public:
     int maxSubarraySumCircular(vector<int>& nums) {
-
-        int totalSum = 0;
-
-        int curMax = 0, maxSum = nums[0];
-        int curMin = 0, minSum = nums[0];
-
-        for(int num : nums) {
-
-            curMax = max(num, curMax + num);
-            maxSum = max(maxSum, curMax);
-
-            curMin = min(num, curMin + num);
-            minSum = min(minSum, curMin);
-
-            totalSum += num;
+        int maxi = maxsubarray(nums);
+        int mini = minsubarray(nums);
+        int tsum =0;
+        for(int i=0;i<nums.size();i++){
+            tsum +=nums[i];
         }
-
-        if(maxSum < 0)
-            return maxSum;
-
-        return max(maxSum, totalSum - minSum);
+        
+        int cirrmax =  tsum -mini;
+        if(maxi > 0){
+            return max(maxi, cirrmax);
+        }
+        return maxi;
+        
     }
 };
