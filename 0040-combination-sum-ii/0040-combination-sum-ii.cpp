6eca6290e2370cf67e14123dfdb43d32d958ccb6
Aiman Nishat <aimannishat78692@gmail.com>
2026-06-26 23:36:27 +0530

    Time: 2 ms (44.24%), Space: 14.1 MB (62.33%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -1,41 +1,39 @@
 class Solution {
 public:
+    vector<vector<int>> result;
 
-    void solve(int idx, vector<int>& arr, int target,
-               vector<int>& temp, vector<vector<int>>& ans){
+    void combisum(vector<int>& candidates,vector<int>& temp,int idx,int target){
 
         if(target == 0){
-            ans.push_back(temp);
+            result.push_back(temp);
             return;
         }
 
-        for(int i = idx; i < arr.size(); i++){
+        if(idx == candidates.size())
+            return;
 
-            // skip duplicates
-            if(i > idx && arr[i] == arr[i-1])
-                continue;
-
-            if(arr[i] > target)
-                break;
-
-            temp.push_back(arr[i]);
-
-            // next index because element use only once
-            solve(i+1, arr, target-arr[i], temp, ans);
-
+        if(candidates[idx] <= target){
+            temp.push_back(candidates[idx]);
+            combisum(candidates, temp, idx + 1,
+                     target - candidates[idx]);
             temp.pop_back();
         }
+
+        int next = idx + 1;
+        while(next < candidates.size() &&
+            candidates[next] == candidates[idx])
+            next++;
+
+        combisum(candidates, temp, next, target);
     }
 
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
 
-        vector<vector<int>> ans;
-        vector<int> temp;
-
         sort(candidates.begin(), candidates.end());
 
-        solve(0, candidates, target, temp, ans);
+        vector<int> temp;
+        combisum(candidates, temp, 0, target);
 
-        return ans;
+        return result;
     }
 };
